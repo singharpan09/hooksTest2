f54da9b277d207b6a6401538ed3cc46fe01c25b1
singharpan <singharpan09@gmail.com>
2020-04-11 12:50:14 +0530

    few changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> this is react.
+          Edit <code>src/App.js</code> this is reactDOM.
         </p>
         <a
           className="App-link"
