f5bb00dd21480a9126126050a812a4951e685819
singharpan <singharpan09@gmail.com>
2020-04-27 13:30:38 +0530

    reducer inialized

---
+++ src/reducers/reducer.jsx
@@ -0,0 +1,4 @@
+//this function could have any name
+//It takes state and action as argument
+//reducer should have some initial data to initalize central store
+const reducer = (state, action) => {};
