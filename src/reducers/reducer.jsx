6eda81f26720bfd0e473d3bf45aba707e9c1e0c2
singharpan <singharpan09@gmail.com>
2020-04-27 13:35:09 +0530

    reducer is inialized with data

--- src/reducers/reducer.jsx
+++ src/reducers/reducer.jsx
@@ -1,4 +1,15 @@
 //this function could have any name
 //It takes state and action as argument
 //reducer should have some initial data to initalize central store
-const reducer = (state, action) => {};
+
+const iState = {
+  name: "Ramesh",
+  wishes: ["eat", "code", "repeat"],
+};
+//initial data for reducer
+
+const reducer = (state = iState, action) => {
+  return state;
+};
+
+export default reducer;
