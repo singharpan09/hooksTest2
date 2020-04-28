cadc439817fa2587ce7ac3a4cc124d7f390473b8
singharpan <singharpan09@gmail.com>
2020-04-28 16:34:12 +0530

    wishreducer patched

--- src/reducers/wishReducer.jsx
+++ src/reducers/wishReducer.jsx
@@ -2,15 +2,8 @@
 //we have empty initial wishes array
 //we send [](type of state we are expecting) ,from all the reducer
 //all the state value combine in "index.js" and make a state object
-
+//any action has not been dispatched to this reducer....so it will simply return state
 const wishReducer = (state = [], action) => {
-  console.log(action);
-  if (action.type === "CHANGE_NAME") {
-    return {
-      ...state,
-      name: action.payload,
-    };
-  }
   return state;
 };
 //else state will be previous state "return state"
