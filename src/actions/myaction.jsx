325a302c698c273f1a7336a04968f73e9f8d9808
singharpan <singharpan09@gmail.com>
2020-04-28 11:10:11 +0530

    action created successfull

--- src/actions/myaction.jsx
+++ src/actions/myaction.jsx
@@ -1,5 +1,6 @@
 //action crator is a function that returns the action
 //It is used for asysnchronous actions...when data takes time to load
+//we make action seperatly here and import and use with dispatch function in "App.js"
 
 export const anotherName = (name) => {
   return {
