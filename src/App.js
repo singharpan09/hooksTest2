2af0228cc1c3b06b0ba73792a7fe0eda6e121eba
singharpan <singharpan09@gmail.com>
2020-05-21 10:01:52 +0530

    useSelector,useDispatch hooks imported from "react-redux"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { anotherName, addWish } from "./actions/myaction";
 //now we will use hooks to get and dispatch values to store
 import "./App.css";
-
+//will use useSelector and useDispatch hooks
+//these hooks are the part of react-redux
 function App(props) {
-  const mywishes = props.mywish.map((item) => {
-    return <h2 key={Math.random()}>{item}</h2>;
-  });
+  // const mywishes = props.mywish.map((item) => {
+  //   return <h2 key={Math.random()}>{item}</h2>;
+  // });
   return (
     <div className="App">
       <h2>This is App component</h2>
@@ -30,27 +31,27 @@ function App(props) {
     </div>
   );
 }
-//to get data "mapStatetoProps"
-const mapStateToProps = (state) => {
-  return {
-    myname: state.name,
-    mywish: state.wish,
-  };
-};
-//we are not sending any data..so no need to send argument to API
-//to change/update data "mapDispatchToProps"
-const mapDispatchToProps = (dispatch) => {
-  return {
-    changeName: () => {
-      dispatch(anotherName());
-    },
-    addWish: () => {
-      dispatch(addWish());
-    },
-  };
-};
+// //to get data "mapStatetoProps"
+// const mapStateToProps = (state) => {
+//   return {
+//     myname: state.name,
+//     mywish: state.wish,
+//   };
+// };
+// //we are not sending any data..so no need to send argument to API
+// //to change/update data "mapDispatchToProps"
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     changeName: () => {
+//       dispatch(anotherName());
+//     },
+//     addWish: () => {
+//       dispatch(addWish());
+//     },
+//   };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
 //parethesis() is used to invoke and App component is passed to it...as "connect" return Higher Order Component
 //If Component is not needed to display but only update the state...then first argument to connect() is null
 
